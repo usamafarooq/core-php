00ee69765cf1d664ac90606acbde160ce5bbd592
sameer <sameeriqbal1200@gmail.com>
2019-05-27 15:34:40 +0500

    UPdate table

--- db/core-php (1).sql
+++ db/core-php (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 21, 2019 at 01:02 PM
+-- Generation Time: May 27, 2019 at 12:33 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.1.26
 
@@ -25,37 +25,47 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `slider`
+-- Table structure for table `client_words`
 --
 
-CREATE TABLE `slider` (
+CREATE TABLE `client_words` (
   `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
-  `button_text` varchar(255) NOT NULL,
-  `button_url` varchar(255) NOT NULL,
+  `client_name` varchar(255) NOT NULL,
+  `client_designation` varchar(255) NOT NULL,
   `image` varchar(255) NOT NULL,
-  `status` int(11) NOT NULL,
+  `status` int(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `slider`
+-- Dumping data for table `client_words`
 --
 
-INSERT INTO `slider` (`id`, `title`, `description`, `button_text`, `button_url`, `image`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'Slide 1', 'Slide 1 description', 'Slide 1 button_text', 'Slide 1 button_url', 'Slide 1 image', 1, '2019-05-21 10:34:40', '2019-05-21 10:34:40');
+INSERT INTO `client_words` (`id`, `title`, `description`, `client_name`, `client_designation`, `image`, `status`, `created_at`, `updated_at`) VALUES
+(16, '1ascascascasc', '2ascascacaa', '3cascascac', '4acascasc', 'home-decor.png', 1, '2019-05-27 10:00:40', '2019-05-27 10:20:19');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `slider`
+-- Indexes for table `client_words`
 --
-ALTER TABLE `slider`
+ALTER TABLE `client_words`
   ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `client_words`
+--
+ALTER TABLE `client_words`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
