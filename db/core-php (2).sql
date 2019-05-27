bb98d4c64057d6fa1be960bcdbf25453e8395caa
sameer <sameeriqbal1200@gmail.com>
2019-05-27 16:02:31 +0500

    s

---
+++ db/core-php (2).sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 27, 2019 at 01:00 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.1.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `core-php`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client_words`
+--
+
+CREATE TABLE `client_words` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `client_name` varchar(255) NOT NULL,
+  `client_designation` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `status` int(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `client_words`
+--
+
+INSERT INTO `client_words` (`id`, `title`, `description`, `client_name`, `client_designation`, `image`, `status`, `created_at`, `updated_at`) VALUES
+(16, '1ascascascasc', '2ascascacaa', '3cascascac', '4acascasc', 'home-decor.png', 1, '2019-05-27 10:00:40', '2019-05-27 10:20:19');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `client_words`
+--
+ALTER TABLE `client_words`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `client_words`
+--
+ALTER TABLE `client_words`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
